b6b85d9549ba5602637ada8eb13f796ed48faaef
Luis00809 <davidcarvajal008@gmail.com>
2023-08-19 12:23:59 -0700

    created classes/test

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const inquirer = require('inquirer')
 const { writeFile } = require('fs/promises');
+const createSVG = require('./lib/document');
+const createDocument = require('./lib/document');
+const Shapes = require('./lib/shapes');
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
 
 inquirer.prompt([
     {
         type: 'input',
         message: 'Plesase provide 3 characters for your logo',
-        name: 'characters'
+        name: 'characters',
+        validate: (characters) => characters.length <= 3 || 'characters are more than 3'
     }, 
     {
         type: 'input',
         message: 'What color would you like to use?',
-        name: 'color'
+        name: 'shapeColor'
+    },
+    {
+        type: 'input',
+        message: 'what text color?',
+        name: 'textColor'
     },
     {
         type: 'list',
         message: 'What shape would you like?',
-        choices: ['Circle', 'Triangle', 'Square'],
-        name: 'shape'
+        choices: ['circle', 'triangle', 'square'],
+        name: 'shapeChoice'
     }
 ]).then ((response ) => {
-    console.log(response.characters);
-    console.log(response.color);
-    console.log(response.shape);
+   
+let shape;
 
-    let logoText = response.characters.slice(0,3);
-    console.log(logoText);
-
-    // writeFile('logo.svg', )
+if(response.shapeChoice === 'circle') {
+    shape = new Circle(response.shapeColor, response.characters, response.textColor )
+}else if(response.shapeChoice === 'triangle'){
+    shape = new Triangle(response.shapeColor, response.characters, response.textColor )
+} else if (response.shapeChoice === 'square'){
+    shape = new Square(response.shapeColor, response.characters, response.textColor )
+}
+console.log(shape)
+const svg = createDocument(shape);
+writeFile('logo.svg', svg, (err) => 
+err ? console.error(err) : console.log('Success'));
 })
